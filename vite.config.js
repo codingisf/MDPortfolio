0f060a9a2dafdbf3df6e018ffec1aed863b025eb
Balaji <balajibalaji8681014395@example.com>
2024-05-24 08:01:39 +0530

    mm

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default {
+  plugins: [react()],
+  root: './',
   build: {
     rollupOptions: {
       external: ['react-router-hash-link']
