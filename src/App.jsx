892d5bcdba365c1db0eb55f452f7bbdf4b619783
Balaji <balajibalaji8681014395@example.com>
2024-05-26 19:51:30 +0530

    solved

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,32 @@ import FirstPage from "./FirstPage/FirstPage";
 import SecondPage from "./SecondPage/SecondPage";
 import ThirdPage from "./ThirdPage/ThirdPage";
 import Fourthpage from "./FourthPage/Fourthpage";
-import { BrowserRouter } from "react-router-dom";
 import Navbar from "./Navbar/Navbar";
 import ScrollBar from "./ScrollBar";
-
+import { Element } from "react-scroll";
 
 function App() {
-
   return (
-    <BrowserRouter>
-      <Navbar/>
-      <FirstPage />
-      <SecondPage />
-      <ThirdPage />
-      <Fourthpage/>
-     <ScrollBar/>
-    </BrowserRouter>
+    <>
+      <div>
+        <Navbar />
+      </div>
+      <div>
+        <Element name="home">
+          <FirstPage />
+        </Element>
+        <Element name="about">
+          <SecondPage />
+        </Element>
+        <Element name="skills">
+          <ThirdPage />
+        </Element>
+        <Element name="contact">
+          <Fourthpage/>
+                 </Element>
+      </div>
+      <ScrollBar />
+    </>
   );
 }
 
--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,30 +1,30 @@
 import React from "react";
-import { HashLink as Link } from "react-router-hash-link";
+import { Link } from "react-scroll";
 
 function Navbar() {
-  
-
-  return <nav>
-  <div className="nav-logo">
-    <h3>Cybernetic MD</h3>
-  </div>
-  <div className="list">
-    <ul className="ul">
-    <Link className="link" smooth to="#home">
-      <li className="li">Home</li>
-      </Link>
-      <Link className="link" smooth to="#about">
-        <li className="li">About</li>
-      </Link>
-      <Link className="link" smooth to="#skills">
-        <li className="li">Skills</li>
-      </Link>
-      <Link className="link" smooth to="#contact">
-        <li className="li">Contact</li>
-      </Link>
-    </ul>
-  </div>
-</nav>;
+  return (
+    <nav>
+      <div className="nav-logo">
+        <h3>Cybernetic MD</h3>
+      </div>
+      <div className="list">
+        <ul className="ul">
+          <Link className="link" to="home" smooth={true} duration={500}>
+            <li className="li">Home</li>
+          </Link>
+          <Link className="link" to="about" smooth={true} duration={700}>
+            <li className="li">About</li>
+          </Link>
+          <Link className="link" to="skills" smooth={true} duration={900}>
+            <li className="li">Skills</li>
+          </Link>
+          <Link className="link" to="contact" smooth={true} duration={1200}>
+            <li className="li">Contact</li>
+          </Link>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
