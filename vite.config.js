bfee206b9c1bff128be25a2de11a5fd78277a3b2
Balaji <93272649+codingisf@users.noreply.github.com>
2024-05-26 20:08:49 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      'react-router-dom': 'react-router-dom',
-      'react-router-hash-link': 'react-router-hash-link'
-    }
-  },
-  build: {
-    rollupOptions: {
-      external: ['react-router-dom', 'react-router-hash-link']
-    }
-  }
+  plugins: [react()]
 });
+
